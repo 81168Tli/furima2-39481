0f779146be6f7430bfc69ce2c239f4b9a3ea3001
81168Tli <135498013+81168Tli@users.noreply.github.com>
2023-06-26 18:41:24 +0900

    ユーザ登録機能の実装、ビューファイルの配置

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname, presence: true
+         validates :lastname1, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+         validates :firstname1, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+         validates :lastname2, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+         validates :firstname2, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+         validates :birth, presence: true
+
+
 end
