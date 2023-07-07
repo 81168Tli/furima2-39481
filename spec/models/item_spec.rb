59a6995a4ad9af85e45c646f22603431684c659a
81168Tli <135498013+81168Tli@users.noreply.github.com>
2023-07-07 22:53:16 +0900

    テストコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,72 +4,80 @@ RSpec.describe Item, type: :model do
   before do
     @user = FactoryBot.create(:user)
     @item = FactoryBot.build(:item, user: @user)
-
   end
 
-  describe 'ユーザー新規登録' do
-      
-    context '新規登録できる場合' do
-      it "全ての値が存在すれば登録できる" do
+  describe '出品' do
+    context '正常系' do
+      it '全ての項目が入力されていれば出品できる' do
         expect(@item).to be_valid
+      end
     end
-  end
-
-    context '新規登録できない場合' do
-
-      it "nameが空では登録できない" do
-        @item.name = ''
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Name can't be blank")
-      end
-
-      it "内容が空では登録できない" do
-        @item.content = ''
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Content can't be blank")
-      end
-
-      it "カテゴリーが空では登録できない" do
-        @item.genre_id  = '1'
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Genre can't be blank")
-      end
-
-      it "状態が空では登録できない" do
-        @item.state_id = ''
-        @item.valid?
-        expect(@item.errors.full_messages).to include("State can't be blank")
-      end
-
-      it "配送料が空では登録できない" do
-        @item.deriver_cost_id = ''
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Deriver cost can't be blank")
-      end
-
-      it "配送元が空では登録できない" do
-        @item.prefecture_id  = ''
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
-      end
-
-      it "配送日が空では登録できない" do
-        @item.deriver_day_id = ''
-        @item.valid?
-
-        expect(@item.errors.full_messages).to include("Deriver day can't be blank")
+    
+    context '異常系' do
+      it '商品画像が空では出品できない' do
+        @item.image = nil
+        expect(@item).not_to be_valid
       end
       
-        it "価格が空では登録できない" do
-          @item.price = ''
-          @item.valid?
-          expect(@item.errors.full_messages).to include("Price can't be blank")
-        end
-
-        
-    
+      it '商品名が空では出品できない' do
+        @item.name = ''
+        expect(@item).not_to be_valid
+      end
+      
+      it '商品説明が空では出品できない' do
+        @item.content = ''
+        expect(@item).not_to be_valid
+      end
+      
+      it 'カテゴリーに「---」が選択されている場合は出品できない' do
+        @item.genre_id = nil
+        expect(@item).not_to be_valid
+      end
+      
+      it '商品の状態に「---」が選択されている場合は出品できない' do
+        @item.state_id = nil
+        expect(@item).not_to be_valid
+      end
+      
+      it '配送料の負担に「---」が選択されている場合は出品できない' do
+        @item.deriver_cost_id = nil
+        expect(@item).not_to be_valid
+      end
+      
+      it '発送元の地域に「---」が選択されている場合は出品できない' do
+        @item.prefecture_id = nil
+        expect(@item).not_to be_valid
+      end
+      
+      it '発送までの日数に「---」が選択されている場合は出品できない' do
+        @item.deriver_day_id = nil
+        expect(@item).not_to be_valid
+      end
+      
+      it '価格が空では出品できない' do
+        @item.price = nil
+        expect(@item).not_to be_valid
+      end
+      
+      it '価格に半角数字以外が含まれている場合は出品できない' do
+        @item.price = '1000a'
+        expect(@item).not_to be_valid
+      end
+      
+      it '価格が300円未満では出品できない' do
+        @item.price = 299
+        expect(@item).not_to be_valid
+      end
+      
+      it '価格が9_999_999円を超えると出品できない' do
+        @item.price = 10_000_000
+        expect(@item).not_to be_valid
+      end
+      
+      it 'userが紐付いていなければ出品できない' do
+        @item.user = nil
+        expect(@item).not_to be_valid
+      end
     end
   end
-
 end
-
