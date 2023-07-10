8ca67ef49bff724c2e0cd445a0c798d846a0db7e
81168Tli <135498013+81168Tli@users.noreply.github.com>
2023-07-10 19:07:53 +0900

    編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :check_owner, only: [:edit, :update]
+
 
   def index
     @items = Item.includes(:user).order(created_at: :desc)
@@ -24,6 +26,22 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if
+    @item.update(item_params)
+    redirect_to item_path(params[:id])
+
+    else
+    render :edit, status: :unprocessable_entity
+    end
+  end
+
+
         
 
     
@@ -33,6 +51,13 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:image, :name, :content, :genre_id, :state_id, :deriver_cost_id, :prefecture_id, :deriver_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def check_owner
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 
 
