d45b736c6bcb024596b2562807dbc84305b4af64
81168Tli <135498013+81168Tli@users.noreply.github.com>
2023-07-10 14:25:00 +0900

    マイグレーション修正

--- db/migrate/20230703050202_create_items.rb
+++ db/migrate/20230703050202_create_items.rb
@@ -11,7 +11,7 @@ class CreateItems < ActiveRecord::Migration[7.0]
       t.integer :deriver_cost_id, null: false
       t.integer :prefecture_id, null: false
       t.integer :deriver_day_id, null: false
-      t.integer :user_id, null: false
+      t.references :user, null: false, foreign_key: true
 
       
       t.timestamps
