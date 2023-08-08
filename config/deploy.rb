3752f61d2512e887faac3c03c274763190552354
81168Tli <135498013+81168Tli@users.noreply.github.com>
2023-08-08 09:41:25 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.17.3"
+lock "3.17.3"
 
 set :application, "furima2-39481"
 set :repo_url,  'git@github.com:81168Tli/furima2-39481.git'
