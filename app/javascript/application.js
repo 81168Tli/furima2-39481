fed91d5086360be0af576bfb9e91738b602cc2a3
81168Tli <135498013+81168Tli@users.noreply.github.com>
2023-08-15 17:06:40 +0900

    プレビュー機能実装

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "item_price"
 import "card"
+import "item_price"
+import "preview"
+
 
 
 
